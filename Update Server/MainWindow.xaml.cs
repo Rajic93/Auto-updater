c3074240db6d9d253e9ba1f3dac3ca98895b7c44
Aleksandar Rajic <aleksandar.v.rajic@gmail.com>
2017-10-24 14:38:53 +0200

    Server finished

--- Update Server/MainWindow.xaml.cs
+++ Update Server/MainWindow.xaml.cs
@@ -23,36 +23,23 @@ namespace Server
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Watcher watcher;
 
         private XmlDocument _xmlDocument;
+
+        private string _manifestLocation = $"{Directory.GetCurrentDirectory()}\\app\\update.xml";
 
         public MainWindow()
         {
             InitializeComponent();
-            watcher = new Watcher(_xmlDocument, UpdateXmlView);
-            watcher.Initialize();
         }
 
         private void UpdateXmlView(XmlDocument doc)
         {
-            
-
             FlowDocument ObjFdoc = new FlowDocument();
-
-            //Add paragraphs to flowdocument Blocks property
-
             Paragraph ObjPara1 = new Paragraph();
-
             ObjPara1.Inlines.Add(new Run(XmlCreator.GetPrettyXml(doc)));
-            
-
             ObjFdoc.Blocks.Add(ObjPara1);
-
-            // Finally Assign the FlowDocuemnt object to Document Property fo RichTextBox Control.
             XmlFile.Document = ObjFdoc;
-            //Update(ObjFdoc);
-
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -65,27 +52,64 @@ namespace Server
         {
 
             XmlNodeList list = _xmlDocument.GetElementsByTagName("update");
-
             (list[0] as XmlElement).SetAttribute("appId", AppId.Text);
             list = list[0].ChildNodes;
+
             foreach (XmlElement xmlElement in list)
             {
                 string value = "";
                 if (xmlElement.Name == "version")
                     value = Version.Text;
-                if (xmlElement.Name == "url")
+                else if (xmlElement.Name == "url")
                     value = Url.Text;
-                if (xmlElement.Name == "exe")
+                else if (xmlElement.Name == "exe")
                     value = Exe.Text;
-                if (xmlElement.Name == "description")
+                else if (xmlElement.Name == "description")
                     value = new TextRange(Description.Document.ContentStart, Description.Document.ContentEnd).Text;
-                if (xmlElement.Name == "launchArgs")
+                else if (xmlElement.Name == "launchArgs")
                     value = new TextRange(Args.Document.ContentStart, Args.Document.ContentEnd).Text;
+                else
+                    continue;
                 xmlElement.SetAttribute("value", value);
             }
 
-            _xmlDocument.Save($"{Directory.GetCurrentDirectory()}\\app\\update.xml");
+            _xmlDocument.Save(_manifestLocation);
             UpdateXmlView(_xmlDocument);
+        }
+
+        private void ButtonBase3_OnClick(object sender, RoutedEventArgs e)
+        {
+            _xmlDocument = new XmlDocument();
+            _xmlDocument.LoadXml(_manifestLocation);
+            XmlNodeList list = _xmlDocument.GetElementsByTagName("update");
+            AppId.Text = (list[0] as XmlElement).GetAttribute("appId");
+            list = list[0].ChildNodes;
+            foreach (XmlElement xmlElement in list)
+            {
+                string value = xmlElement.GetAttribute("value");
+                if (xmlElement.Name == "version")
+                    Version.Text = value;
+                else if (xmlElement.Name == "url")
+                    Url.Text = value;
+                else if (xmlElement.Name == "exe")
+                    Exe.Text = value;
+                else if (xmlElement.Name == "description")
+                {
+                    FlowDocument ObjFdoc = new FlowDocument();
+                    Paragraph ObjPara1 = new Paragraph();
+                    ObjPara1.Inlines.Add(value);
+                    ObjFdoc.Blocks.Add(ObjPara1);
+                    Description.Document = ObjFdoc;
+                }
+                else if (xmlElement.Name == "launchArgs")
+                {
+                    FlowDocument ObjFdoc = new FlowDocument();
+                    Paragraph ObjPara1 = new Paragraph();
+                    ObjPara1.Inlines.Add(value);
+                    ObjFdoc.Blocks.Add(ObjPara1);
+                    Args.Document = ObjFdoc;
+                }
+            }
         }
     }
 }
